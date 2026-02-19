8f6775b51bff9d99f0baa7a508d4f04afc818823
Geetika Saini <geetikasaini2024@gmail.com>
2026-02-19 10:45:47 +0000

    Initial Commit

---
+++ cracker.py
@@ -0,0 +1,43 @@
+import hashlib
+def typeofhash(hash):
+    if(hash==256):
+        return 'SHA256'
+    elif(hash==128):
+        return 'MD5'
+    else:
+        return 'SHA1' #160
+def lengthofhash(hash):
+    len_hash=len(hash)*4
+    hashtype=typeofhash(len_hash)
+    return hashtype
+def conversion(password,hash_type):
+    if(hash_type=='SHA256'):
+        hash_value=hashlib.SHA256(str.encode(password)).hexdigest()
+        return hash_value
+    elif(hash_type=='MD5'):
+        hash_value=hashlib.md5(str.encode(password)).hexdigest()
+        return hash_value
+    else:
+        hash_value=hashlib.sha1(str.encode(password)).hexdigest()
+        return hash_value
+    
+def main():
+    get_hash=input("Enter the hash for which u want to get password: ")
+    hash_type=lengthofhash(get_hash)
+    with open("top-10000-passwords.txt","r") as f:
+        for password in f:
+            data=password[:-1]
+            with open("known-salts.txt","r") as s:
+                for salt in s:
+                    pre=salt[:-1]
+                    check_list=[data,pre+data,data+pre,pre+data+pre]
+                    for i in check_list:
+                        data_hash=conversion(i,hash_type)
+                        if(data_hash==get_hash):
+                            print(f'Password is {data}')
+                            break
+
+                
+if __name__ =="__main__":
+    main()
+
