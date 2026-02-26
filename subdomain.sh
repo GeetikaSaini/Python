c5a0bc3f222c04756fa1a395c4d78f436e628f25
Geetika Saini <geetikasaini2024@gmail.com>
2026-02-26 03:46:46 +0000

    Subfinder added

--- subdomain.sh
+++ subdomain.sh
@@ -10,9 +10,22 @@ check_amass() {
     fi
 }
 
+check_subfinder() {
+    if ! command -v subfinder &> /dev/null; then
+        echo "Subfinder is not installed. Installing..."
+        sudo apt install golang -y
+        wget https://go.dev/dl/go1.24.0.linux-amd64.tar.gz
+        sudo tar -C /usr/local -xzf go1.24.0.linux-amd64.tar.gz
+        source ~/.bashrc
+        go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+    else
+        echo "Already installed."
+    fi
+}
+
 perform_amass_enum() {
     check_amass
-    if [ ! -d "$domain/amass" ]; then          # ← space before ]
+    if [ ! -d "$domain/amass" ]; then          
         mkdir -p "$domain/amass"
         echo "Created folder: amass"
     else
@@ -22,10 +35,22 @@ perform_amass_enum() {
     output_file="$domain/amass/amass_${domain}_${datetime}.txt"
     echo "Saving results to: $output_file"
     amass enum -active -d "$domain" -r 8.8.8.8 2>/dev/tty | tee "$output_file"
+    echo "Amass enumeration completed. Results saved to: $output_file"
 }
 
 perform_subfinder_enum() {
-    echo "Perform subdomain enumeration using subfinder tool"
+    check_subfinder
+    if [ ! -d "$domain/subfinder" ]; then          
+        mkdir -p "$domain/subfinder"
+        echo "Created folder: subfinder"
+    else
+        echo "Folder already exists"
+    fi
+    datetime=$(date +%Y-%m-%d_%H-%M-%S)
+    output_file="$domain/subfinder/subfinder_${domain}_${datetime}.txt"
+    echo "Saving results to: $output_file"
+    subfinder -d "$domain" -all -recursive  2>/dev/tty | tee "$output_file"
+    echo "Subfinder enumeration completed. Results saved to: $output_file"
 }
 
 choose_tool() {
@@ -36,7 +61,6 @@ choose_tool() {
     case $choice in
         1) perform_amass_enum ;;
         2) perform_subfinder_enum ;;
-        *) echo "Invalid choice" ;;
     esac
 }
 
